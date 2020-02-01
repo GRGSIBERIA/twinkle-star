da4b197216150b54661cea61bb196bfc581e4aae
Eiichi Takebuchi <nanashi4129@gmail.com>
2020-02-01 23:06:32 +0900

    整理して神クラスを作る

---
+++ twinkle-star/God.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#include <Siv3D/Color.hpp>
+
+namespace tw
+{
+	const Font font(24);
+
+	const Color dark_color(U"#994311");
+	const Color font_color(U"#DEC418");
+	const Color accent_color(U"#DE1895");
+	const Color theme_color(U"#18DFC4");
+	const Color sad_color(U"#119987");
+}
